e582e151e547de434482073c7fd0324a6dfc5c7f
Muhammad Ryan Sofiyulloh <89008846+ryansof22@users.noreply.github.com>
2026-06-03 16:31:32 +0700

    Refactor props definition and add computed properties

--- js/components/stock-table.js
+++ js/components/stock-table.js
@@ -1,8 +1,30 @@
 Vue.component('ba-stock-table', {
     template: '#tpl-stock',
-    props: {
-        items: { type: Array, required: true },
-        upbjjList: { type: Array, required: true },
-        kategoriList: { type: Array, required: true }
+    props: ['items', 'upbjjList', 'kategoriList'],
+    data() {
+        return {
+            filterUPBJJ: '',
+            filterKategori: '',
+            sortKey: ''
+        };
+    },
+    computed: {
+        filteredItems() {
+            let data = this.items.filter(i => {
+                return (this.filterUPBJJ === '' || i.upbjj === this.filterUPBJJ) &&
+                       (this.filterKategori === '' || i.kategori === this.filterKategori);
+            });
+            if (this.sortKey) {
+                data.sort((a, b) => a[this.sortKey] > b[this.sortKey] ? 1 : -1);
+            }
+            return data;
+        }
+    },
+    methods: {
+        resetFilter() { this.filterUPBJJ = ''; this.filterKategori = ''; },
+        sortBy(key) { this.sortKey = key; },
+        deleteItem(item) {
+            this.$root.$refs.modal.show(`Menghapus ${item.judul}...`);
+        }
     }
 });
