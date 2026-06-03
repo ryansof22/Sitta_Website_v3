472512e2384816d560a7719e46e9dd5d2c9c54c3
Muhammad Ryan Sofiyulloh <89008846+ryansof22@users.noreply.github.com>
2026-06-03 18:02:53 +0700

    Refactor app-modal component for better promise handling

--- js/components/app-modal.js
+++ js/components/app-modal.js
@@ -1,13 +1,27 @@
 Vue.component('app-modal', {
-    template: '#tpl-modal',
-    data() { return { visible: false, message: '' }; },
-    methods: {
-        // Ganti show(msg) menjadi:
-        open(title, message, onConfirm) {
-        this.title = title;
-        this.message = message;
-        this.onConfirmCallback = onConfirm;
-        this.isOpen = true; // Pastikan data di app-modal.js memiliki property 'isOpen'
-        }
+  template: '#tpl-modal',
+  data() {
+    return {
+      isOpen: false,
+      title: '',
+      message: '',
+      resolvePromise: null
+    };
+  },
+  methods: {
+    show(title, message) {
+      this.title = title;
+      this.message = message;
+      this.isOpen = true;
+      return new Promise((resolve) => {
+        this.resolvePromise = resolve;
+      });
+    },
+    close(result) {
+      this.isOpen = false;
+      if (this.resolvePromise) {
+        this.resolvePromise(result);
+      }
     }
+  }
 });
